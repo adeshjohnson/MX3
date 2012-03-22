523e35db93412e90b306f2040893a0f8655bc040
aisteb <aisteb@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2012-03-22 06:30:09 +0000

    fix tests
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@22468 19941b1f-7a74-413c-ab52-0de3e92e9a87

---
+++ selenium/tests/sql/ivr_list.sql
@@ -0,0 +1,8 @@
+INSERT INTO `ivrs` (`id`, `name`, `start_block_id`, `user_id`) VALUES
+(2, 'aaaaivrs', 2, 0),
+(3, 'baaaivrs', 3, 0),
+(4, 'abaaivrs', 4, 0),
+(5, 'zaaaivrs', 5, 0),
+(6, 'aaaaiv rs', 6, 0),
+(7, '!& aaaaivrs', 7, 0),
+(8, 'aaa aivrs', 8, 0);
