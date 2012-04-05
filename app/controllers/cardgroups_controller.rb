7540eb7f881fd79434115377f00dbdf06276c2f6
vitalija <vitalija@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2012-04-05 07:52:37 +0000

    #5515 BRAIN 2 Class: NoMethodError Message: undefined method `only' for {"controller"=>"cardgroups", "action"=>"search"}:ActiveSupport::HashWithIndifferentAccess Controller: cardgroups Action: search User ID: 0
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@22918 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- app/controllers/cardgroups_controller.rb
+++ app/controllers/cardgroups_controller.rb
@@ -78,7 +78,7 @@ class CardgroupsController < ApplicationController
       "s_sold" => "",
       "s_caller_id" => ''
     }
-    @options.merge!(session[:cardgroup_search_options]).merge!(params.only(*@options.keys))
+    @options.merge!(session[:cardgroup_search_options]).merge!(params.slice(*@options.keys))
     session[:cardgroup_search_options] = @options
     @page = params[:page].to_i
     @cards, @card_count = Card.search(corrected_user_id, @options, { :page => @page, :per_page => session[:items_per_page] })
