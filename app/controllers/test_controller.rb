9f4fbb08ef097947b3b67b1d0c6906893e1cee11
vitalija <vitalija@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2012-04-11 15:21:27 +0000

    exceptions.case
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@23222 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 class TestController < ApplicationController
-  before_filter :authorize_admin, :except=>[:fake_form]
+  before_filter :authorize_admin, :except => [:fake_form]
 
   def create_user
     #    opts = {}
@@ -19,51 +19,51 @@ class TestController < ApplicationController
   end
 
   def raise_exception
-    params[:this_is_fake_exception] = "YES"
+    params[:this_is_fake_exception] = nil
     case params[:id]
-    when "Errno::ENETUNREACH"
-      raise Errno::ENETUNREACH
-    when "Transactions"
-      raise ActiveRecord::Transactions::TransactionError, 'Transaction aborted'
-    when "RuntimeError"
-      raise RuntimeError, 'No route to host'
-    when "Errno::EHOSTUNREACH"
-      raise Errno::EHOSTUNREACH
-    when "Errno::ETIMEDOUT"
-      raise Errno::ETIMEDOUT
-    when "SystemExit"
-      raise SystemExit
-    when"SocketError"
-      raise SocketError
-    when "NoMemoryError"
-      raise NoMemoryError
-    when "DNS_TEST"
-      raise 'getaddrinfo: Temporary failure in name resolution'
-    when "SyntaxError"
-      raise SyntaxError
-    when "OpenSSL::SSL::SSLError"
-      Confline.set_value("Last_Crash_Exception_Class", "")
-      raise OpenSSL::SSL::SSLError
-    when "Cairo"
-      params[:this_is_fake_exception] = nil
-      raise LoadError, 'Could not find the ruby cairo bindings in the standard locations or via rubygems. Check to ensure they\'re installed correctly'
-    when "Gems"
-      raise LoadError, 'in the standard locations or via rubygems. Check to en'
-    when "MYSQL"
-      Confline.set_value("Last_Crash_Exception_Class", "")
-      sql = "alter table users drop first_name ;"
-      test = ActiveRecord::Base.connection.execute(sql)
-      us = User.find(0)
-      us.first_name
-    when "test_exceptions"
-      Confline.set_value("Last_Crash_Exception_Class", "")
-      params[:this_is_fake_exception] = ""
-      raise SyntaxError
-    when "pdf_limit"
-      PdfGen::Count.check_page_number(4, 1)
+      when "Errno::ENETUNREACH"
+        raise Errno::ENETUNREACH
+      when "Transactions"
+        raise ActiveRecord::Transactions::TransactionError, 'Transaction aborted'
+      when "RuntimeError"
+        raise RuntimeError, 'No route to host'
+      when "Errno::EHOSTUNREACH"
+        raise Errno::EHOSTUNREACH
+      when "Errno::ETIMEDOUT"
+        raise Errno::ETIMEDOUT
+      when "SystemExit"
+        raise SystemExit
+      when "SocketError"
+        raise SocketError
+      when "NoMemoryError"
+        params[:this_is_fake_exception] = "YES"
+        raise NoMemoryError
+      when "DNS_TEST"
+        raise 'getaddrinfo: Temporary failure in name resolution'
+      when "SyntaxError"
+        raise SyntaxError
+      when "OpenSSL::SSL::SSLError"
+        Confline.set_value("Last_Crash_Exception_Class", "")
+        raise OpenSSL::SSL::SSLError
+      when "Cairo"
+        raise LoadError, 'Could not find the ruby cairo bindings in the standard locations or via rubygems. Check to ensure they\'re installed correctly'
+      when "Gems"
+        raise LoadError, 'in the standard locations or via rubygems. Check to en'
+      when "MYSQL"
+        Confline.set_value("Last_Crash_Exception_Class", "")
+        sql = "alter table users drop first_name ;"
+        test = ActiveRecord::Base.connection.execute(sql)
+        us = User.find(0)
+        us.first_name
+      when "test_exceptions"
+        Confline.set_value("Last_Crash_Exception_Class", "")
+        params[:this_is_fake_exception] = ""
+        raise SyntaxError
+      when "pdf_limit"
+        PdfGen::Count.check_page_number(4, 1)
     end
   end
-  
+
   def nice_exception_raiser
     params[:this_is_fake_exception] = "YES"
     if params[:exc_class]
@@ -72,15 +72,15 @@ class TestController < ApplicationController
   end
 
   def last_exception
-    render :text => Confline.get_value("Last_Crash_Exception_Class",0).to_s
+    render :text => Confline.get_value("Last_Crash_Exception_Class", 0).to_s
   end
 
   def remove_duplicates
     i = 0
     sql = "SELECT user_id, data, target_id, count(*) as 'size' FROM actions WHERE action = 'subscription_paid' GROUP BY user_id, data, target_id HAVING size > 1"
     duplicates = ActiveRecord::Base.connection.execute(sql)
-    duplicates.each{ |dub|
-      (dub[3].to_i - 1).times{
+    duplicates.each { |dub|
+      (dub[3].to_i - 1).times {
         action = Action.find(:first, :include => [:user], :conditions => ["action = 'subscription_paid' AND user_id = ? AND data = ? AND target_id = ?", dub[0].to_i, dub[1], dub[2].to_i])
         user = action.user
         user.balance += action.data2.to_f
@@ -95,7 +95,7 @@ class TestController < ApplicationController
 
   def load_delta_sql
     MorLog.my_debug(params[:path])
-   # MorLog.my_debug(params[:path].join("/"))
+    # MorLog.my_debug(params[:path].join("/"))
     MorLog.my_debug(File.exist?("#{Rails.root}/config/routes.rb"))
     filename = "#{Rails.root}/selenium/tests/#{params[:path].to_s.gsub(/[^A-Za-z_\/]/, "")}.sql"
     MorLog.my_debug(filename)
@@ -117,7 +117,7 @@ class TestController < ApplicationController
   end
 
   def fake_form
-    @all_fields = params.reject{|key, value| ['controller', 'action', 'path_to_action'].include?(key)}
+    @all_fields = params.reject { |key, value| ['controller', 'action', 'path_to_action'].include?(key) }
     @data = params[:path_to_action]
   end
 
@@ -135,9 +135,9 @@ class TestController < ApplicationController
       render :text => "script not found"
     end
   end
-  
+
   def test_api
-    allow , values = MorApi.check_params_with_all_keys(params, request)
+    allow, values = MorApi.check_params_with_all_keys(params, request)
     render :text => values[:system_hash]
   end
 
@@ -147,13 +147,13 @@ class TestController < ApplicationController
 
     @table = @tables.include?(params[:table]) ? params[:table] : nil
 
-    if params[:id] and @table 
+    if params[:id] and @table
       if @table.to_s != 'sessions'
-        @select = @table.singularize.titleize.gsub(' ', '').constantize.find(:first, :conditions=>{:id => params[:id]})
+        @select = @table.singularize.titleize.gsub(' ', '').constantize.find(:first, :conditions => {:id => params[:id]})
       else
         @select = ActiveRecord::Base.connection.select_all("SELECT * FROM #{params[:table]} WHERE id = #{params[:id].to_i}")
       end
-    end  
+    end
   end
 
 end
