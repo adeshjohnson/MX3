12916c4d8c4c4d71c232f1f79e39f86cb06067e8
vitalija <vitalija@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2012-05-29 13:25:05 +0000

    Test: payments/payments.case
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@24859 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- app/models/cardgroup.rb
+++ app/models/cardgroup.rb
@@ -140,7 +140,7 @@ class Cardgroup < ActiveRecord::Base
     logger.fatal new_tax.to_yaml
     new_tax.save if options[:save] == true
     self.tax_id = new_tax.id
-    logger.fatal "ffffffffffffffffffffffffffffffffff#{tax_id}fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+    logger.fatal "ffffffffffffffffffffffffffffffffff#{self.tax_id}fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
     self.save if options[:save] == true
   end
 
