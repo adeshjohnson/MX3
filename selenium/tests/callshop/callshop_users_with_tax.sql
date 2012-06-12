504a1760b8790b997428a11a59ae5ebeb9ccc266
arunasj <arunasj@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2012-06-12 14:00:48 +0000

    Test Fix
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@25467 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- selenium/tests/callshop/callshop_users_with_tax.sql
+++ selenium/tests/callshop/callshop_users_with_tax.sql
@@ -1,3 +1,5 @@
+/*group*/
+insert into `groups` values (10,'Test_shop','callshop',0,'',1);
 /* taxes*/
 INSERT INTO `taxes` (`id`,`tax1_enabled`,`tax2_enabled`,`tax3_enabled`,`tax4_enabled`,`tax1_name`,`tax2_name`,`tax3_name`,`tax4_name`,`total_tax_name`,`tax1_value`,`tax2_value`,`tax3_value`,`tax4_value`,`compound_tax`)
 VALUES              (17  ,1             ,0             ,0             ,0             ,'Tax1'     ,''         ,''         ,''         ,'Taxx'          ,60          ,0           ,0           ,0           ,1),
