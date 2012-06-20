7ccf78dff159a0f09c05e370513bc13083760018
vitalija <vitalija@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2012-06-20 12:47:38 +0000

    Test: callshop/callshop_only_user_as_manager.case
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@25824 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- app/models/user.rb
+++ app/models/user.rb
@@ -3017,7 +3017,7 @@ GROUP BY terminators.id;").map { |t| t.id }
   attribute manual helps to sovle this issue.
 =end
   def minimal_charge_start_at=(value)
-    value = (value ? value.to_s(:db) : value)
+    value = (value ? value.to_s : value)
     write_attribute(:minimal_charge_start_at, value)
   end
 
