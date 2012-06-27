16aed363d2880da3f622b6a59e619333fff26235
aisteb <aisteb@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2012-06-27 08:19:45 +0000

    #5982 - BUG su valiutomis
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@26025 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- selenium/tests/sql/update_currencies.sql
+++ selenium/tests/sql/update_currencies.sql
@@ -1,3 +1,5 @@
 update currencies set exchange_rate=0.5 where id=2;
 update currencies set exchange_rate=2, active=1 where id=3;
 update currencies set exchange_rate=4, active=1 where id=4;
+update currencies set name ='LABAS' where id =143;
+update currencies set exchange_rate=9, active=1 where id=143;
