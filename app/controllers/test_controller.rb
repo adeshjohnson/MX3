3131a82e059fa502efcdbdfcefccd28c4d6b2d27
arunasj <arunasj@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2012-07-23 11:45:33 +0000

    Test Fix
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@27162 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -99,10 +99,11 @@ class TestController < ApplicationController
   end
 
   def load_delta_sql
-    MorLog.my_debug(params[:path])
+    path = (params[:path].to_s.empty? ? ('sql/'+params[:id]) : params[:path])
+    MorLog.my_debug(path)
     # MorLog.my_debug(params[:path].join("/"))
     MorLog.my_debug(File.exist?("#{Rails.root}/config/routes.rb"))
-    filename = "#{Rails.root}/selenium/tests/#{params[:path].to_s.gsub(/[^A-Za-z_\/]/, "")}.sql"
+    filename = "#{Rails.root}/selenium/tests/#{path.to_s.gsub(/[^A-Za-z_\/]/, "")}.sql"
     MorLog.my_debug(filename)
     if File.exist?(filename)
       command = "mysql -u mor -pmor mor < #{filename}"
