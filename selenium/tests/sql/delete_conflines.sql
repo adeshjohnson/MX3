7f55311394ead1a8686ac6340c7c8533c472747b
aisteb <aisteb@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2012-07-26 14:47:37 +0000

    fix tests
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@27367 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- selenium/tests/sql/delete_conflines.sql
+++ selenium/tests/sql/delete_conflines.sql
@@ -1,1 +1,1 @@
-delete from conflines where name like '%action%';;
+delete from conflines where name='daily_actions_cooldown_time' or name='monthly_actions_cooldown_time' or name='hourly_actions_cooldown_time';
