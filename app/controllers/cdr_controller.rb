7aaca4d540e95afed0ea511fc2b7c6a2710d4950
aurimass <aurimass@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2012-08-28 12:53:09 +0000

    CLI Add
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@28588 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- app/controllers/cdr_controller.rb
+++ app/controllers/cdr_controller.rb
@@ -279,7 +279,7 @@ class CdrController < ApplicationController
 
     logger.fatal @dev
     logger.fatal @cli
-    unless @dev or @cli
+    if !@dev or !@cli or @dev == nil or @cli == nil
       @error = _('Device_or_Cli_not_found')
       @users = User.find(:all, :select => "users.*, #{SqlExport.nice_user_sql}", :joins => "JOIN devices ON (users.id = devices.user_id)", :conditions => "hidden = 0 and devices.id > 0 AND owner_id = #{correct_owner_id}", :order => "nice_user ASC", :group => 'users.id')
     else
