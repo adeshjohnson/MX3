5da3bee031be6930974b83ca668334ee0af67b9a
aurimass <aurimass@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2012-09-24 14:27:42 +0000

    #6518 12: bugfix
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@29975 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- app/controllers/dialplans_controller.rb
+++ app/controllers/dialplans_controller.rb
@@ -126,7 +126,7 @@ class DialplansController < ApplicationController
     end
 
     # checking if selected user belongs to current user.
-    if User.find(:first, :conditions => "id = '#{params[:user].to_i}'").owner_id.to_i != current_user.id.to_i and !current_user.is_admin?
+    if User.find(:first, :conditions => ['id =  ? AND owner_id != ? AND ? != 0 ', params[:user], current_user.id, current_user.id]) and !current_user.is_admin?
        flash[:notice] = _('Dont_be_so_smart')
        redirect_to :action => 'new' and return false
     end
