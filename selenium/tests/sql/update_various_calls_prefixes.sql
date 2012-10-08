733c76b3ba1aa1c80bbcc9a61a0ae871edc77a4f
aisteb <aisteb@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2012-10-08 14:07:17 +0000

    update_various_calls_prefixes.sql
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@30469 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- selenium/tests/sql/update_various_calls_prefixes.sql
+++ selenium/tests/sql/update_various_calls_prefixes.sql
@@ -2,3 +2,4 @@ update calls set prefix='370' where id between 211 and 218;
 update calls set prefix='3705' where id between 221 and 224;
 update calls set prefix='370' where id between 272 and 273;
 update calls set prefix='3705' where id in (275,277);
+update calls set did_prov_id=0;
