7eff87d3ccaa440a52d816d30cef1deb44718b05
vitalija <vitalija@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2012-10-11 11:34:59 +0000

    #6627 MA naudoja Production DB
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@30614 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- lib/scripts/monitoring_script.rb
+++ lib/scripts/monitoring_script.rb
@@ -3,7 +3,6 @@
 
 require '/home/mor/lib/sql_export'
 include SqlExport
-require 'uri'
 
 #---------- check that script is not running ------------------------------------
 script_running = `ps ax | grep monitoring_script.rb | grep -v grep | wc -l`
@@ -15,6 +14,7 @@ else
   require 'active_record'
   require 'optparse'
   require 'digest/sha1'
+  require 'uri'
 
   options = {}
   optparse = OptionParser.new do |opts|
