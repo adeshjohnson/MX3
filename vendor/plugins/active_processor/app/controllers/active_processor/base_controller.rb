5ed6f226c592f95ed314d2c4dbbb1bdc6b0de819
aurimass <aurimass@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2012-10-16 11:42:29 +0000

    #6641
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@30765 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- vendor/plugins/active_processor/app/controllers/active_processor/base_controller.rb
+++ vendor/plugins/active_processor/app/controllers/active_processor/base_controller.rb
@@ -64,7 +64,7 @@ module ActiveProcessor
         else
           @gateway = @engine.query
           format.html {
-            flash.now[:notice] = _('Payment_Error') + " <small>#{@gateway.payment.response.try(:message) unless @gateway.payment.nil?}</small>"
+            flash.now[:notice] = _('Payment_Error') + " <small>#{(@gateway.payment.response.try(:message) rescue "") unless @gateway.payment.nil?}</small>"
             render :action => "index"
           }
         end
