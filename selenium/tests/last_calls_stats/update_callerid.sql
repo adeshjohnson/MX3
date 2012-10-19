7325399390a36562ac68952d2b0212762db3b35f
tester2 <tester2@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2012-10-19 11:00:11 +0000

    update_callerid.sql
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@30863 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- selenium/tests/last_calls_stats/update_callerid.sql
+++ selenium/tests/last_calls_stats/update_callerid.sql
@@ -1,4 +1,4 @@
 update calls set clid='"labas" <labas>' where user_id=0;
 update calls set clid='"labas" <labas>' where user_id=2;
 update calls set clid='"labas" <labas>' where user_id=3;
-update calls set clid='"labas" <labas>' where user_id=4;
+update calls set clid='"labas" <labas>' where user_id=4; 
