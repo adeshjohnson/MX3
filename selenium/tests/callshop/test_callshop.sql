612d3b3fae652926102e71acff130b7469efd499
tester2 <tester2@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2012-10-26 14:25:45 +0000

    test_callshop.sql
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@31174 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- selenium/tests/callshop/test_callshop.sql
+++ selenium/tests/callshop/test_callshop.sql
@@ -1,7 +1,11 @@
-INSERT INTO `groups` (`id`, `name`, `grouptype`) VALUES  (1, 'All users', 'simple'); 
+INSERT INTO `groups` (`id`, `name`, `grouptype`) VALUES  
+(1, 'All users', 'simple'),
+(2, 'Test_shop', 'callshop'); 
 
 INSERT INTO `usergroups` (`id`, `user_id`, `group_id`, `gusertype`) VALUES 
 	 	(1, 0, 1, 'manager'), 
 	 	(2, 1, 1, 'user'), 
 	 	(3, 2, 1, 'user'), 
 	 	(4, 3, 1, 'user');
+
+
