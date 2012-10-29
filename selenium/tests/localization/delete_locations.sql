50c7e952a38e51eae96d79e993fccd90a81507c3
aisteb <aisteb@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2012-10-29 11:57:04 +0000

    delete_locations.case
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@31227 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- selenium/tests/localization/delete_locations.sql
+++ selenium/tests/localization/delete_locations.sql
@@ -1,3 +1,3 @@
-delete locationrules.* from locationrules where location_id in (select id from locations where user_id = 3);
-delete locations.* from locations where user_id = 3;
-delete conflines.* from conflines where owner_id = 3 and  name = 'Default_device_location_id';
+delete locationrules.* from locationrules where location_id in (select id from locations where user_id in (3,10,11,12,13));
+delete locations.* from locations where user_id  in (3,10,11,12,13);
+delete conflines.* from conflines where owner_id in (3,10,11,12,13) and  name = 'Default_device_location_id';
