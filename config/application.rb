0eebe5e784a570c5c42081dc82aa58540dfb605c
vitalija <vitalija@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2012-11-08 13:17:53 +0000

    #6732 [S8] ROR 3 warnings
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@31593 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,9 @@ module Mor
      config.active_record.default_timezone = :local
      config.active_record.time_zone_aware_attributes = false
 
+    # disable warnings
+    ActiveSupport::Deprecation.silenced = true
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
--- vendor/plugins/active_processor/init.rb
+++ vendor/plugins/active_processor/init.rb
@@ -6,8 +6,8 @@
     ActiveSupport::Dependencies.autoload_paths << path
     ActiveSupport::Dependencies.autoload_once_paths.delete(path)
     # <hack>
-    HOST = IO.readlines(File.dirname(__FILE__) + '/../../../config/environment.rb').reject { |l| l =~ /^#/ }.join.match(/[^#]Web_URL ?= ?"(\S+)"/)[1]
-    HOST = $1
+    @aHOST = IO.readlines(File.dirname(__FILE__) + '/../../../config/environment.rb').reject { |l| l =~ /^#/ }.join.match(/[^#]Web_URL ?= ?"(\S+)"/)[1]
+    @aHOST = $1
     # </hack>
   else
     # <hack>
@@ -23,7 +23,7 @@ def initialize_production(config)
 
   config.after_initialize do
     ActiveProcessor.configure do |config|
-      config.host = HOST
+      config.host = @aHOST
       config.translate_func = lambda { |s| Localization::_t(s, ActiveProcessor.configuration.language) }
       config.calculate_tax = lambda { |u, a| User.find(:first, :conditions => {:id => u}).get_tax.apply_tax(a) }
       config.substract_tax = lambda { |u, a| User.find(:first, :conditions => {:id => u}).get_tax.count_amount_without_tax(a) }
@@ -37,7 +37,7 @@ def initialize_development(config)
     ActiveMerchant::Billing::Base.mode = :test
 
     ActiveProcessor.configure do |config|
-      config.host = HOST
+      config.host = @aHOST
       config.translate_func = lambda { |s| Localization::_t(s, ActiveProcessor.configuration.language) }
       config.currency_exchange = lambda { |c1, c2| Currency.count_exchange_rate(c1, c2) }
       config.calculate_tax = lambda { |u, a| User.find(:first, :conditions => {:id => u}).get_tax.apply_tax(a) }
