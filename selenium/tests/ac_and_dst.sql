769676356e4e033a89cda1af07d624291e9408fe
tester2 <tester2@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2012-11-26 06:04:30 +0000

    commit to 12
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@32075 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- selenium/tests/ac_and_dst.sql
+++ selenium/tests/ac_and_dst.sql
@@ -3,5 +3,5 @@ INSERT INTO `activecalls`
   (24  ,    1,      '1249296551.111096',   NOW(),        NOW(),          NULL,        '555333327342','55533307889', 11,              0,               'SIP/10.219.62.200-c40daf10','',           '555333',     1,              0,       9,         0,          '55533307889');
 
 INSERT INTO `destinations` (`id`,`prefix`,`direction_code`,`subcode`,`name`     ,`city`,`state`,`lata`,`tier`,`ocn`,`destinationgroup_id`)
-VALUES                    (200431, 555333 ,''             ,'MOB '   , 'testini_s' , NULL , NULL  , NULL , NULL , NULL,                   0);
+VALUES                    (200431, 555333 ,''             ,'MOB '   , 'testini_s' , NULL , NULL  , NULL , NULL , NULL,                   0); 
 
