4d0da68959d54127b3d99d1b74c7ae9b5c9b4618
aurimass <aurimass@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2012-11-30 10:20:35 +0000

    #6816 - 12
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@32252 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- app/controllers/cdr_controller.rb
+++ app/controllers/cdr_controller.rb
@@ -484,13 +484,13 @@ class CdrController < ApplicationController
 
           for call in @calls
             provider = providers_cache["p_#{call.provider_id}".to_sym] ||= Provider.find(:first, :include => [:tariff], :conditions => ["providers.id = ?", call.provider_id])
-            if provider and provider.user_id == current_user.get_corrected_owner_id
+            grace_time = Device.select("grace_time").where(:id => call.src_device_id).first
+            if provider and provider.user_id == current_user.get_corrected_owner_id and (grace_time.blank? or grace_time < call.billsec)
 
               one_old_user_price += call.user_price.to_d
               one_old_reseller_price += call.reseller_price.to_d
               one_old_provider_price += call.provider_price.to_d
             
-              logger.fatal 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
               logger.fatal one_old_user_price
               logger.fatal one_old_reseller_price
               logger.fatal one_old_provider_price
@@ -523,7 +523,6 @@ class CdrController < ApplicationController
           @old_reseller_price += one_old_reseller_price.to_d
           @old_user_price += one_old_user_price.to_d
 
-          logger.fatal 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
           logger.fatal @old_user_price
           logger.fatal @old_reseller_price
           logger.fatal @old_provider_price
