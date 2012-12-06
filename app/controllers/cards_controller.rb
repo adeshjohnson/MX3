e053a87a79412c6ad1d9a850fdf7068fa0878791
dmitrija <dmitrija@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2012-12-06 12:09:01 +0000

    #6881 [S9] Korteliu trinimas uzlockino DB
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@32450 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -397,7 +397,6 @@ class CardsController < ApplicationController
       sql = "INSERT INTO cards (`user_id`, `balance`, `cardgroup_id`, `sold`, `number`, `pin`, `owner_id`, `language`) VALUES "
       for n in start_num..end_num
         @card = Card.select("cards.*, cardgroups.name AS 'ccg_name'").where(["number = ?", n]).joins("LEFT JOIN cardgroups ON (cards.cardgroup_id = cardgroups.id)").first
-        i += 1
         if @card.blank? and !card_pins[i].to_s.blank?
           cards_created += 1
           sql << "('#{params[:user_id]}','#{@cg.price}','#{@cg.id}','#{false}','#{n}','#{card_pins[i]}','#{owner_id}','#{params[:card_language]}'"
@@ -415,6 +414,7 @@ class CardsController < ApplicationController
           end
           @cards_with_errors << @card
         end
+        i += 1
       end
       if i > 0
         ActiveRecord::Base.connection.execute(sql)
