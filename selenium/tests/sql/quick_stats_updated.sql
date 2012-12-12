9bce12a4db9152d137bbaceb546cd9ecfd63c3f6
tester2 <tester2@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2012-12-12 08:52:38 +0000

    quick stats sql
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@32567 19941b1f-7a74-413c-ab52-0de3e92e9a87

---
+++ selenium/tests/sql/quick_stats_updated.sql
@@ -0,0 +1,17 @@
+update calls set calldate='2007-12-31 18:00:04' where id=20;
+update calls set calldate='2007-12-31 18:00:05' where id=23;
+update calls set calldate='2009-01-01 18:00:01' where id=9;
+update calls set calldate='2009-01-01 18:00:02' where id=10;
+update calls set calldate='2009-01-01 18:00:03' where id=11;
+update calls set calldate='2009-01-01 18:00:04' where id=12;
+update calls set calldate='2009-01-02 18:00:01' where id=13;
+update calls set calldate='2009-01-02 18:00:02' where id=14;
+update calls set calldate='2009-01-02 18:00:03' where id=15;
+update calls set calldate='2009-01-02 18:00:04' where id=16;
+
+INSERT INTO `devices` VALUES
+(13, '1004', 'dynamic', '6kh2p1mu2n45', 'mor_local', '', 5060, 0, 13, NULL, '1004', 0, '1004', 'SIP', 4, 0, 1, 0, 0, 'no', 'all', 'alaw;g729', '0.0.0.0/0.0.0.0', '0.0.0.0/0.0.0.0', 'yes', '1000', NULL, 'no', 2, 'rfc2833', NULL, NULL, '', '', 'no', 'no', '', 'no', 'no', 1, 'sadasdasd', 0, 0, '718519', 0, 0, 60, 60, 'no', NULL, 0, 'no', 60, 0, NULL, 0, 0, 0, 'yes', 'yes', 0, NULL, 0, 0, '', 0, 0, 0, '', 1, 0, '', 'no', 'en', 0, '', 0, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 'udp', 'no', '', 0, 0);
+
+insert into calls values
+(38, '2007-12-25 18:00:04', '37046246362', '37046246362', '37063042438', '', '', '', '', '', 40, 50, 'ANSWERED', 0, '2', '1232113377.3', '', 13, 0, 0, 0, 0, 1, 0, 0, 1, -1, 0, 1, 5, 3, 0, 0, 4, 0, 0, 0, 0, '3706', 0, 16, 'Outside', '', '', '', '', '', '', 0, 4, 4, '37063042438', 0, 0, '', '', 0, 0, 50, NULL);
+
