07d21dc18d8209effd5fadda3b9b76258ee8e5cb
tester2 <tester2@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2012-12-12 08:59:57 +0000

    quick stats sql
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@32568 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- selenium/tests/sql/quick_stats_updated.sql
+++ selenium/tests/sql/quick_stats_updated.sql
@@ -9,7 +9,7 @@ update calls set calldate='2009-01-02 18:00:02' where id=14;
 update calls set calldate='2009-01-02 18:00:03' where id=15;
 update calls set calldate='2009-01-02 18:00:04' where id=16;
 
-INSERT INTO `devices` VALUES
+INSERT INTO devices VALUES
 (13, '1004', 'dynamic', '6kh2p1mu2n45', 'mor_local', '', 5060, 0, 13, NULL, '1004', 0, '1004', 'SIP', 4, 0, 1, 0, 0, 'no', 'all', 'alaw;g729', '0.0.0.0/0.0.0.0', '0.0.0.0/0.0.0.0', 'yes', '1000', NULL, 'no', 2, 'rfc2833', NULL, NULL, '', '', 'no', 'no', '', 'no', 'no', 1, 'sadasdasd', 0, 0, '718519', 0, 0, 60, 60, 'no', NULL, 0, 'no', 60, 0, NULL, 0, 0, 0, 'yes', 'yes', 0, NULL, 0, 0, '', 0, 0, 0, '', 1, 0, '', 'no', 'en', 0, '', 0, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 'udp', 'no', '', 0, 0);
 
 insert into calls values
