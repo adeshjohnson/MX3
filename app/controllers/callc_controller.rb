225c5a2777575a3427b9bab9571dd58ef7d6cd06
dmitrija <dmitrija@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2012-12-17 11:50:20 +0000

    #6972 Message: undefined method `get_hash' for nil:NilClass
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@32654 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- app/controllers/callc_controller.rb
+++ app/controllers/callc_controller.rb
@@ -216,7 +216,7 @@ class CallcController < ApplicationController
 
     flash[:notice] = _('logged_off')
     if Confline.get_value("Logout_link", owner_id).to_s.blank?
-      if owner.get_hash()
+      if !owner.blank? and owner.get_hash()
         redirect_to :action => "login", :id => owner.get_hash()
       else
         redirect_to :action => "login"
