47f40c2014b83f724329863faa9a7316aa991835
dmitrija <dmitrija@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2013-01-14 17:01:58 +0000

    #7001 D BUG: Invoice in USD, shows in EUR
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@33328 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -565,7 +565,7 @@ class Invoice < ActiveRecord::Base
           aa << {:text => dc} if type == 3
           items << aa
         end
-        tax_amount += self.nice_invoice_number(self.price_with_vat * ex, nice_number_hash.merge({:no_repl => 1})).to_d
+        tax_amount += self.nice_invoice_number(self.converted_price_with_vat(ex), nice_number_hash.merge({:no_repl => 1})).to_d
 
       }
       price_with_tax = tax_amount.to_d
