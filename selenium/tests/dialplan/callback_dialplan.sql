e1c03b50c610841e99f1b232163663539111ff4c
tester2 <tester2@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2013-01-22 08:23:33 +0000

    callback_dialplan moved to dialplan folder
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@33533 19941b1f-7a74-413c-ab52-0de3e92e9a87

---
+++ selenium/tests/dialplan/callback_dialplan.sql
@@ -0,0 +1,3 @@
+INSERT INTO `dialplans` (`id`,`name`             ,`dptype`  ,`data1`,`data2`,`data3`,`data4`,`data5`,`data6`,`data7`,`data8`,`sound_file_id`,`user_id`)
+VALUES                  ( 100,'Callback_dialplan','callback',1      ,10     ,1000000,1      ,NULL   ,NULL   ,NULL   ,NULL   ,0              ,0); 
+
