001f0ef0189a6b62f614ebe27b1ab939792d867e
ieva <ieva@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2013-01-29 09:00:10 +0000

    backup tests fixed
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@33722 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- selenium/tests/sql/backups.sql
+++ selenium/tests/sql/backups.sql
@@ -1,4 +1,6 @@
 INSERT INTO `backups` (`id`,`backuptime`     ,`comment`                     ,`backuptype`) VALUES
+                      (  1 ,'20081122145553' , 'Dabar2008-11-22 14:55:53'   , 'manual'),
+                      (  2 ,'20091122145553' , 'Dabar2009-11-22 14:55:53'   , 'manual'),
                       (  3 ,'20101122145553' , 'Dabar2010-11-22 14:55:53'   , 'manual'),
                       (  4 ,'20111122145553' , 'Dabar2011-11-22 14:55:53'   , 'manual'),
 		      (  77,'20101103154533' , 'not_exiting'                , 'manual');
