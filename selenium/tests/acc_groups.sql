b0b17bdebea29e97ab85e268622aba6b85bb5c06
ieva <ieva@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2013-02-08 07:44:50 +0000

    commit to 12, 12.126, x4
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@33973 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- selenium/tests/acc_groups.sql
+++ selenium/tests/acc_groups.sql
@@ -1,4 +1,4 @@
 INSERT INTO `acc_groups`(`id`,`name`                  ,`group_type`) VALUES
                         ( 11 ,'Accountant_permissions','accountant'),
-                        ( 12 ,'Reseller_Permissions'  ,'reseller'); 
+                        ( 12 ,'Reseller_Permissions'  ,'reseller');   
  
