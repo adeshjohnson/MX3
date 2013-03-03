8fd9e5326327b6d79ebf4f31576d968d92999e20
aurimass <aurimass@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2013-03-03 01:27:14 +0000

    fix for test export
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@34733 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1010,8 +1010,8 @@ in before filter : user (:find_user_from_id_or_session)
         filename = Call.last_calls_csv(options)
         filename = load_file_through_database(filename) if Confline.get_value("Load_CSV_From_Remote_Mysql").to_i == 1
         if filename
-          filename = archive_file_if_size(filename, "csv", Confline.get_value("CSV_File_size").to_d)
           if params[:test].to_i != 1
+            filename = archive_file_if_size(filename, "csv", Confline.get_value("CSV_File_size").to_d)
             file = File.open(filename)
             send_data file.read, :filename => filename
           else
@@ -1227,8 +1227,8 @@ in before filter : user (:find_user_from_id_or_session, :authorize_user)
       filename = Call.country_stats_csv({:collumn_separator => Confline.get_csv_separator(settings_owner_id), :s_user => @user_id, :current_user => current_user, :from => session_from_datetime, :till => session_till_datetime, :nice_number_digits => session[:nice_number_digits], :test => params[:test].to_i, :hide_finances => !can_see_finances?})
       filename = load_file_through_database(filename) if Confline.get_value("Load_CSV_From_Remote_Mysql").to_i == 1
       if filename
-        filename = archive_file_if_size(filename, "csv", Confline.get_value("CSV_File_size").to_d)
         if params[:test].to_i != 1
+          filename = archive_file_if_size(filename, "csv", Confline.get_value("CSV_File_size").to_d)
           send_data(File.open(filename).read, :filename => filename)
         else
           render :text => filename
@@ -1279,8 +1279,8 @@ in before filter : user (:find_user_from_id_or_session, :authorize_user)
     filename = @user.user_calls_to_csv({:tz => current_user.time_offset, :device => @device, :direction => @direction, :call_type => call_type, :date_from => date_from, :date_till => date_till, :default_currency => session[:default_currency], :show_currency => session[:show_currency], :show_full_src => session[:show_full_src], :hgc => @hgc, :usertype => user_type, :nice_number_digits => session[:nice_number_digits], :test => params[:test].to_i, :reseller => res.to_i, :hide_finances => !can_see_finances?})
     filename = load_file_through_database(filename) if Confline.get_value("Load_CSV_From_Remote_Mysql").to_i == 1
     if filename
-      filename = archive_file_if_size(filename, "csv", Confline.get_value("CSV_File_size").to_d)
       if params[:test].to_i != 1
+        filename = archive_file_if_size(filename, "csv", Confline.get_value("CSV_File_size").to_d)
         send_data(File.open(filename).read, :filename => filename)
       else
         render :text => filename
@@ -2517,8 +2517,8 @@ in before filter : user (:find_user_from_id_or_session, :authorize_user)
     @orig_call_type = @call_type
 
     filename = provider.provider_calls_csv({:tz => current_user.time_offset, :direction => @direction, :call_type => @call_type, :date_from => date_from, :date_till => date_till, :default_currency => session[:default_currency], :show_currency => session[:show_currency], :show_full_src => session[:show_full_src], :nice_number_digits => session[:nice_number_digits], :test => params[:test].to_i})
-    filename = archive_file_if_size(filename, "csv", Confline.get_value("CSV_File_size").to_d)
     if params[:test].to_i != 1
+      filename = archive_file_if_size(filename, "csv", Confline.get_value("CSV_File_size").to_d)
       send_data(File.open(filename).read, :filename => filename)
     else
       render :text => filename
