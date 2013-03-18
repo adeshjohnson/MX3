b68bfbd49c41adee5bcb6e39d7ff31e7986bfc8d
aurimass <aurimass@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2013-03-18 14:04:01 +0000

    #7139 - x3 - qf rules mess
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@35159 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- app/models/quickforwards_rule.rb
+++ app/models/quickforwards_rule.rb
@@ -50,9 +50,9 @@ class QuickforwardsRule < ActiveRecord::Base
       regexp = rule_regexp.delete('%')
     end
 
-    User.current.usertype == "admin" ? cond = " OR dids.reseller_id != 0 " : cond = ""
+    User.current.usertype == "admin" ? cond = "dids.user_id != 0 AND" : User.current.usertype == "reseller" ? cond = "reseller_id = #{User.current.id} AND" : cond = ""
 
-    dids = Did.includes(:dialplan).where("dids.did REGEXP('^(#{regexp})') AND (dialplans.dptype != 'quickforwarddids' OR (dialplans.id IS NULL AND status = 'reserved') #{cond})")
+    dids = Did.includes(:dialplan).where("dids.did REGEXP('^(#{regexp})') AND #{cond} (dialplans.dptype != 'quickforwarddids' OR (dialplans.id IS NULL AND status = 'reserved'))")
     if dids and dids.size.to_i > 0
       errors.add(:prefix,_('Collisions_with_Dids'))
       return false
