49f3cb6f4421da1078e9f731b917d5c4b38a76e5
dmitrija <dmitrija@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2013-03-21 08:09:12 +0000

    #7124 D [S15] [Optional] DID rate + CC
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@35295 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -101,7 +101,7 @@ class CallsController < ApplicationController
       terminator_billsec = "SUM(IF(calls.disposition = 'ANSWERED', calls.reseller_billsec, 0)) AS 'terminating_billsec'"
     else
       # Check if call belongs to resellers user if yes then admins income is reseller perice
-      originating_billed = SqlExport.replace_price("SUM(IF(users.owner_id = 0 AND calls.disposition = 'ANSWERED', if(calls.user_price is NULL, 0, #{SqlExport.user_price_sql}), if(calls.reseller_price IS NULL, 0, calls.reseller_price)))", {:reference => 'originating_billed'})
+      originating_billed = SqlExport.replace_price("SUM(IF(users.owner_id = 0 AND calls.disposition = 'ANSWERED', if(calls.user_price is NULL, 0, #{SqlExport.user_price_sql}), if(calls.reseller_price IS NULL, 0, (calls.reseller_price + calls.did_inc_price))))", {:reference => 'originating_billed'})
       originating_billsec = "SUM(IF(users.owner_id = 0 AND calls.disposition = 'ANSWERED', IF(calls.user_billsec IS NULL, 0, calls.user_billsec), if(calls.reseller_billsec IS NULL, 0, calls.reseller_billsec))) AS 'originating_billsec'"
 
       terminator_billed = SqlExport.replace_price("SUM(IF(calls.disposition = 'ANSWERED', #{SqlExport.admin_provider_price_sql}, 0)) - calls.did_prov_price", {:reference => 'terminating_billed'})
