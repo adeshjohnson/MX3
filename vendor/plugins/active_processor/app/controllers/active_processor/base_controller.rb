96079d003502a1b540cfd9b6d4a1c620e5090e16
dmitrija <dmitrija@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2013-04-18 11:36:20 +0000

    #7486 D [S19] HSBC sutvarkymas
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@36183 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- vendor/plugins/active_processor/app/controllers/active_processor/base_controller.rb
+++ vendor/plugins/active_processor/app/controllers/active_processor/base_controller.rb
@@ -67,7 +67,12 @@ module ActiveProcessor
             if (@gateway.errors.size + @gateway.credit_card.errors.size) > 0
               flash.now[:notice] = _('ERRORs') + ":"
             else
-              flash.now[:notice] = _('Payment_Error') + "<br/> * ".html_safe + @gateway.payment.response.params["error_message"].to_s
+              flash.now[:notice] = _('Payment_Error')
+              if @gateway.payment.response.params["return_message"]
+                flash.now[:notice] += "<br/> * ".html_safe + @gateway.payment.response.params["return_message"].to_s
+              else
+                flash.now[:notice] += "<br/> * ".html_safe + @gateway.payment.response.params["error_message"].to_s
+              end
             end
             notice_flash_errors(@gateway.credit_card) if @gateway.credit_card.errors.size > 0
             notice_flash_errors(@gateway) if @gateway.errors.size > 0
