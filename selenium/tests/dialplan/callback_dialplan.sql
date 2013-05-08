d60d276ab0b9826287beeff86560ee20409c35b8
ieva <ieva@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2013-05-08 10:21:08 +0000

    #7607 - dialplans.case
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@36676 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- selenium/tests/dialplan/callback_dialplan.sql
+++ selenium/tests/dialplan/callback_dialplan.sql
@@ -1,3 +1,4 @@
 INSERT INTO `dialplans` (`id`,`name`             ,`dptype`  ,`data1`,`data2`,`data3`,`data4`,`data5`,`data6`,`data7`,`data8`,`sound_file_id`,`user_id`)
-VALUES                  ( 100,'Callback_dialplan','callback',1      ,10     ,1000000,1      ,NULL   ,NULL   ,NULL   ,NULL   ,0              ,0); 
+VALUES                  ( 100,'Callback_dialplan','callback',1      ,10     ,1000000,1      ,NULL   ,NULL   ,NULL   ,NULL   ,0              ,0),
+ ( 101,'reseller_Callback_dialplan','callback',1      ,10     ,1000000,1      ,NULL   ,NULL   ,NULL   ,NULL   ,0              ,3); 
 
