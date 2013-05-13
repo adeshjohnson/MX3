5dd345acdb7145ac9be903fe9039410e6034f7dc
aisteb <aisteb@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2013-05-13 13:48:23 +0000

    #7390 - BUG in Invoice generate
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@36845 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- selenium/tests/sql/update_calls_set_zero_price.sql
+++ selenium/tests/sql/update_calls_set_zero_price.sql
@@ -1,5 +1,5 @@
-UPDATE `calls` SET `user_price` = '0' WHERE `calls`.`id` =33 LIMIT 1 ;
-UPDATE `calls` SET `user_price` = '0' WHERE `calls`.`id` =39 LIMIT 1 ;
-UPDATE `calls` SET `user_price` = '0' WHERE `calls`.`id` =45 LIMIT 1 ;
-UPDATE `calls` SET `user_price` = '0' WHERE `calls`.`id` =48 LIMIT 1 ;
-UPDATE `calls` SET `user_price` = '0' WHERE `calls`.`id` =54 LIMIT 1 ;
+UPDATE `calls` SET `user_price` = 0 WHERE `calls`.`id` =33 LIMIT 1 ;
+UPDATE `calls` SET `user_price` = 0 WHERE `calls`.`id` =39 LIMIT 1 ;
+UPDATE `calls` SET `user_price` = 0 WHERE `calls`.`id` =45 LIMIT 1 ;
+UPDATE `calls` SET `user_price` = 0 WHERE `calls`.`id` =48 LIMIT 1 ;
+UPDATE `calls` SET `user_price` = 0 WHERE `calls`.`id` =54 LIMIT 1 ;
