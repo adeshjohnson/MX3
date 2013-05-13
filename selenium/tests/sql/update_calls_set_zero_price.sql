a34fdbf63c62d4c02e5bffb87eb82aa112b8b109
aisteb <aisteb@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2013-05-13 15:43:31 +0000

    #7390 - BUG in Invoice generate
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@36863 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- selenium/tests/sql/update_calls_set_zero_price.sql
+++ selenium/tests/sql/update_calls_set_zero_price.sql
@@ -1,5 +1,18 @@
+# Admin user calls through provider
 UPDATE `calls` SET `user_price` = 0 WHERE `calls`.`id` =33 LIMIT 1 ;
 UPDATE `calls` SET `user_price` = 0 WHERE `calls`.`id` =39 LIMIT 1 ;
 UPDATE `calls` SET `user_price` = 0 WHERE `calls`.`id` =45 LIMIT 1 ;
 UPDATE `calls` SET `user_price` = 0 WHERE `calls`.`id` =48 LIMIT 1 ;
 UPDATE `calls` SET `user_price` = 0 WHERE `calls`.`id` =54 LIMIT 1 ;
+# Reseller user calls through provider
+UPDATE `calls` SET `user_price` = 0 WHERE `calls`.`id` =30 LIMIT 1 ;
+UPDATE `calls` SET `user_price` = 0 WHERE `calls`.`id` =32 LIMIT 1 ;
+UPDATE `calls` SET `user_price` = 0 WHERE `calls`.`id` =36 LIMIT 1 ;
+UPDATE `calls` SET `user_price` = 0 WHERE `calls`.`id` =43 LIMIT 1 ;
+UPDATE `calls` SET `user_price` = 0 WHERE `calls`.`id` =52 LIMIT 1 ;
+# Reseller user calls through DID
+UPDATE `calls` SET `did_inc_price` = 0 WHERE `calls`.`id` =30 LIMIT 1 ;
+UPDATE `calls` SET `did_inc_price` = 0 WHERE `calls`.`id` =32 LIMIT 1 ;
+UPDATE `calls` SET `did_inc_price` = 0 WHERE `calls`.`id` =36 LIMIT 1 ;
+UPDATE `calls` SET `did_inc_price` = 0 WHERE `calls`.`id` =43 LIMIT 1 ;
+UPDATE `calls` SET `did_inc_price` = 0 WHERE `calls`.`id` =52 LIMIT 1 ;
