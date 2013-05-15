8deff4ed333c493fb0607526a4853d9bb8a0484a
dmitrija <dmitrija@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2013-05-15 14:07:21 +0000

    #7656 D [S22] BUG Message: undefined method `priority' for nil:NilClass
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@36959 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- app/models/lcrprovider.rb
+++ app/models/lcrprovider.rb
@@ -1,7 +1,6 @@
 # -*- encoding : utf-8 -*-
 class Lcrprovider < ActiveRecord::Base
-=begin
-=end
+
   def self.clone_providers(lcr, original_lcr_id)
     query = "INSERT INTO lcrproviders (lcr_id, provider_id, active, priority, percent)
              SELECT #{lcr.id}, lcrproviders.provider_id, lcrproviders.active, lcrproviders.priority, lcrproviders.percent 
@@ -12,13 +11,15 @@ class Lcrprovider < ActiveRecord::Base
 
   def move_lcr_prov(direction)
     if direction == "down"
-      following_lcr_prov = Lcrprovider.where(:lcr_id => self.lcr_id, :priority => self.priority + 1).first
-      self.update_attribute(:priority, self.priority + 1)
-      following_lcr_prov.update_attribute(:priority, following_lcr_prov.priority - 1)
+      following_lcr_prov = Lcrprovider.where("lcr_id = #{self.lcr_id} AND priority > #{self.priority}").order("priority").first
+      old_priority = self.priority
+      self.update_attribute(:priority, following_lcr_prov.priority) if following_lcr_prov
+      following_lcr_prov.update_attribute(:priority, old_priority) if following_lcr_prov
     else
-      previous_lcr_prov = Lcrprovider.where(:lcr_id => self.lcr_id, :priority => self.priority - 1).first
-      self.update_attribute(:priority, self.priority - 1)
-      previous_lcr_prov.update_attribute(:priority, previous_lcr_prov.priority + 1) if previous_lcr_prov
+      previous_lcr_prov = Lcrprovider.where("lcr_id = #{self.lcr_id} AND priority < #{self.priority}").order("priority DESC").first
+      old_priority = self.priority
+      self.update_attribute(:priority, previous_lcr_prov.priority) if previous_lcr_prov
+      previous_lcr_prov.update_attribute(:priority, old_priority) if previous_lcr_prov
     end
   end
 end
