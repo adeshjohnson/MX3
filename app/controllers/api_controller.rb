cd950e568acdeceb63f0b95ebfdc4a209d4ccfa1
aurimass <aurimass@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2013-05-21 10:39:31 +0000

    #7695 - x3 - api wholesale_tariff
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@37155 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -13,7 +13,7 @@ class ApiController < ApplicationController
   before_filter :check_send_method, :except => [:simple_balance, :balance]
   before_filter :log_access
   before_filter :find_current_user_for_api, :only => [:user_subscriptions, :user_invoices, :personal_payments, :user_rates, :callflow_edit, :devices_callflow, :user_devices, :main_page, :logout, :cc_by_cli, :create_payment, :payments_list, :show_calling_card_group, :buy_card_from_callingroup, :financial_statements]
-  before_filter :check_api_parrams_with_hash, :only => [:show_calling_card_group, :buy_card_from_callingroup, :cc_by_cli, :financial_statements, :logout, :user_details, :user_register, :user_update_api, :callback, :invoices, :balance, :simple_balance, :user_balance_change, :rate, :get_tariff, :import_tariff_retail]
+  before_filter :check_api_parrams_with_hash, :only => [:show_calling_card_group, :buy_card_from_callingroup, :cc_by_cli, :financial_statements, :logout, :user_details, :user_register, :user_update_api, :callback, :invoices, :balance, :simple_balance, :user_balance_change, :rate, :get_tariff, :import_tariff_retail, :wholesale_tariff]
   before_filter :check_calling_card_addon, :only => [:show_calling_card_group, :cc_by_cli, :buy_card_from_callingroup]
   before_filter :check_sms_addon, :only => [:send_sms]
 
@@ -2287,12 +2287,10 @@ class ApiController < ApplicationController
   end
 
   def wholesale_tariff
-    allow, values = MorApi.check_params_with_all_keys(params, request)
     doc = Builder::XmlMarkup.new(:target => out_string = "", :indent => 2)
     doc.instruct! :xml, :version => "1.0", :encoding => "UTF-8"
     doc.page {
-    if allow == true
-      check_user(params[:u], params[:p])
+      check_user(params[:u])
       if @user and (@user.is_admin? or @user.is_reseller? or (@user.is_accountant? and @user.accountant_allow_edit('Tariff_manage')))
         tariff_id = params[:id].to_i
         if tariff_id == 0
@@ -2332,9 +2330,6 @@ class ApiController < ApplicationController
       else
         doc.error('Bad login')
       end
-    else
-      doc.error('Incorrect hash')
-    end
     }
     send_xml_data(out_string, params[:test].to_i)
   end
