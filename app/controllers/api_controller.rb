ba5da0862a353af2152700b16db1a935272af8f4
aurimass <aurimass@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2013-05-21 05:52:34 +0000

    #7685 - x3 - api user_update_api
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@37117 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -13,7 +13,7 @@ class ApiController < ApplicationController
   before_filter :check_send_method, :except => [:simple_balance, :balance]
   before_filter :log_access
   before_filter :find_current_user_for_api, :only => [:user_subscriptions, :user_invoices, :personal_payments, :user_rates, :callflow_edit, :devices_callflow, :user_devices, :main_page, :logout, :cc_by_cli, :create_payment, :payments_list, :show_calling_card_group, :buy_card_from_callingroup, :financial_statements]
-  before_filter :check_api_parrams_with_hash, :only => [:show_calling_card_group, :buy_card_from_callingroup, :cc_by_cli, :financial_statements, :logout, :user_details, :user_register]
+  before_filter :check_api_parrams_with_hash, :only => [:show_calling_card_group, :buy_card_from_callingroup, :cc_by_cli, :financial_statements, :logout, :user_details, :user_register, :user_update_api]
   before_filter :check_calling_card_addon, :only => [:show_calling_card_group, :cc_by_cli, :buy_card_from_callingroup]
   before_filter :check_sms_addon, :only => [:send_sms]
 
@@ -1937,12 +1937,10 @@ class ApiController < ApplicationController
   end
 
   def user_update_api
-    allow, values =MorApi.check_params_with_all_keys(params, request)
     doc = Builder::XmlMarkup.new(:target => out_string = "", :indent => 2)
     doc.instruct! :xml, :version => "1.0", :encoding => "UTF-8"
     doc.page {
-    if allow == true
-      check_user(params[:u], params[:p])
+      check_user(params[:u])
       if @user
         if @user.usertype == 'user' or (@user.usertype == 'accountant' and (values[:user_id].to_i == 0 or values[:user_id].to_i == @user.id))
           doc.error(_('Dont_be_so_smart'))
@@ -2054,9 +2052,6 @@ class ApiController < ApplicationController
       else
         doc.error("Bad login")
       end
-    else
-      doc.error("Incorrect hash")
-    end
                        }
     send_xml_data(out_string, params[:test].to_i)
   end
