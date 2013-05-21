d391f4dff48e0137db7f8ee94bd6da0edcabfaeb
aurimass <aurimass@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2013-05-21 13:07:10 +0000

    #7708 - x3 - api credit_note_update
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@37201 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -17,7 +17,7 @@ class ApiController < ApplicationController
 		:only => [:show_calling_card_group, :buy_card_from_callingroup, :cc_by_cli, :financial_statements, :logout, :user_details, :user_register,
 			  :user_update_api, :callback, :invoices, :balance, :simple_balance, :user_balance_change, :rate, :get_tariff, :import_tariff_retail,
 			  :wholesale_tariff, :device_create, :device_destroy, :device_list, :did_create, :did_assign_device, :did_unassign_device, :ma_activate,
-			  :phonebooks, :phonebook_edit, :payments_list, :credit_notes]
+			  :phonebooks, :phonebook_edit, :payments_list, :credit_notes, :credit_note_update]
 
   before_filter :check_calling_card_addon, :only => [:show_calling_card_group, :cc_by_cli, :buy_card_from_callingroup]
   before_filter :check_sms_addon, :only => [:send_sms]
@@ -3021,12 +3021,10 @@ class ApiController < ApplicationController
   /api/credit_note_update?u=user&p=user1&credit_note_id=XXX
 =end
   def credit_note_update
-    allow, values = MorApi.check_params_with_all_keys(params, request)
     doc = Builder::XmlMarkup.new(:target => out_string = "", :indent => 2)
     doc.instruct! :xml, :version => "1.0", :encoding => "UTF-8"
     doc.page {
-    if allow == true
-      check_user(params[:u], params[:p])
+      check_user(params[:u])
       if @user and (@user.is_admin? or @user.is_reseller? or (@user.is_accountant? and @user.accountant_allow_read('invoices_manage')))
         if @user.is_reseller?
           condition = ['users.owner_id = ? AND credit_notes.id = ?', @user.id, params[:credit_note_id].to_i]
@@ -3056,9 +3054,6 @@ class ApiController < ApplicationController
       else
         doc.error("Bad login")
       end
-    else
-      doc.error("Incorrect hash")
-    end
     }
     send_xml_data(out_string, params[:test].to_i)
   end
