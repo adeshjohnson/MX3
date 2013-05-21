cbe3ef2b247ec9a3169b3e7ce1808f8ccd52157d
ieva <ieva@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2013-05-21 06:44:03 +0000

    device weak password tests fixed
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@37124 19941b1f-7a74-413c-ab52-0de3e92e9a87

---
+++ selenium/tests/sql/device_weak_password.sql
@@ -0,0 +1,6 @@
+UPDATE `devices` SET `secret` = '101' WHERE `id`=2;
+UPDATE `devices` SET `secret` = 'test' WHERE `id`=1;
+UPDATE `devices` SET `secret` = '102' WHERE `id`=3;
+UPDATE `devices` SET `secret` = '103' WHERE `id`=5;
+UPDATE `devices` SET `secret` = '104' WHERE `id`=6;
+UPDATE `devices` SET `secret` = '105' WHERE `id`=7;
