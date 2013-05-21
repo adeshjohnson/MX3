a2f26737afe44d07b73ce5d42c3a165bf33bded0
aurimass <aurimass@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2013-05-21 12:24:38 +0000

    #7703 - x3 - api ma_activate
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@37187 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -13,7 +13,7 @@ class ApiController < ApplicationController
   before_filter :check_send_method, :except => [:simple_balance, :balance]
   before_filter :log_access
   before_filter :find_current_user_for_api, :only => [:user_subscriptions, :user_invoices, :personal_payments, :user_rates, :callflow_edit, :devices_callflow, :user_devices, :main_page, :logout, :cc_by_cli, :create_payment, :payments_list, :show_calling_card_group, :buy_card_from_callingroup, :financial_statements]
-  before_filter :check_api_parrams_with_hash, :only => [:show_calling_card_group, :buy_card_from_callingroup, :cc_by_cli, :financial_statements, :logout, :user_details, :user_register, :user_update_api, :callback, :invoices, :balance, :simple_balance, :user_balance_change, :rate, :get_tariff, :import_tariff_retail, :wholesale_tariff, :device_create, :device_destroy, :device_list, :did_create, :did_assign_device, :did_unassign_device]
+  before_filter :check_api_parrams_with_hash, :only => [:show_calling_card_group, :buy_card_from_callingroup, :cc_by_cli, :financial_statements, :logout, :user_details, :user_register, :user_update_api, :callback, :invoices, :balance, :simple_balance, :user_balance_change, :rate, :get_tariff, :import_tariff_retail, :wholesale_tariff, :device_create, :device_destroy, :device_list, :did_create, :did_assign_device, :did_unassign_device, :ma_activate]
   before_filter :check_calling_card_addon, :only => [:show_calling_card_group, :cc_by_cli, :buy_card_from_callingroup]
   before_filter :check_sms_addon, :only => [:send_sms]
 
@@ -1511,19 +1511,17 @@ class ApiController < ApplicationController
 =end
 
   def ma_activate
-    allow, values =MorApi.check_params_with_all_keys(params, request)
     doc = Builder::XmlMarkup.new(:target => out_string = "", :indent => 2)
     users = []
     doc.instruct! :xml, :version => "1.0", :encoding => "UTF-8"
-    if allow == true
       if defined?(MA_Active) and MA_Active == 1
-        if (values.keys & [:monitoring_id, :users, :block, :email, :mtype]).size == 5
-          monitoring = Monitoring.where(:id => values[:monitoring_id], :block => (values[:block] == "true") ? true : false, :email => (values[:email] == "true") ? true : false, :mtype => values[:mtype]).first
+        if (@values.keys & [:monitoring_id, :users, :block, :email, :mtype]).size == 5
+          monitoring = Monitoring.where(:id => @values[:monitoring_id], :block => (@values[:block] == "true") ? true : false, :email => (@values[:email] == "true") ? true : false, :mtype => @values[:mtype]).first
           if monitoring
             doc.status {
               doc.monitoring_found("success")
               doc.users {
-                values[:users].split(",").each { |id|
+                @values[:users].split(",").each { |id|
                   user = User.where(:id => id).first
                   doc.user {
                     doc.id(id)
@@ -1606,11 +1604,6 @@ class ApiController < ApplicationController
           doc.error("Monitorings addon is disabled")
         }
       end
-    else
-      doc.status {
-        doc.error("Incorrect hash")
-      }
-    end
     send_xml_data(out_string, params[:test].to_i)
   end
 
