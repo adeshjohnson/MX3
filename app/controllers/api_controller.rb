528aa9596c9b4e1f8c4c793990cdd2eb53c2fcf5
aurimass <aurimass@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2013-05-21 10:13:06 +0000

    #7694 - x3 - api import_tariff_retail
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@37151 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -13,7 +13,7 @@ class ApiController < ApplicationController
   before_filter :check_send_method, :except => [:simple_balance, :balance]
   before_filter :log_access
   before_filter :find_current_user_for_api, :only => [:user_subscriptions, :user_invoices, :personal_payments, :user_rates, :callflow_edit, :devices_callflow, :user_devices, :main_page, :logout, :cc_by_cli, :create_payment, :payments_list, :show_calling_card_group, :buy_card_from_callingroup, :financial_statements]
-  before_filter :check_api_parrams_with_hash, :only => [:show_calling_card_group, :buy_card_from_callingroup, :cc_by_cli, :financial_statements, :logout, :user_details, :user_register, :user_update_api, :callback, :invoices, :balance, :simple_balance, :user_balance_change, :rate, :get_tariff]
+  before_filter :check_api_parrams_with_hash, :only => [:show_calling_card_group, :buy_card_from_callingroup, :cc_by_cli, :financial_statements, :logout, :user_details, :user_register, :user_update_api, :callback, :invoices, :balance, :simple_balance, :user_balance_change, :rate, :get_tariff, :import_tariff_retail]
   before_filter :check_calling_card_addon, :only => [:show_calling_card_group, :cc_by_cli, :buy_card_from_callingroup]
   before_filter :check_sms_addon, :only => [:send_sms]
 
@@ -2069,7 +2069,7 @@ class ApiController < ApplicationController
     doc = Builder::XmlMarkup.new(:target => out_string4 = "", :indent => 2)
     doc.instruct! :xml, :version => "1.0", :encoding => "UTF-8"
     #check if user exists
-    @user = check_user(params[:u], params[:p])
+    check_user(params[:u])
     collision_rates = []
     collision_rates_with_db = []
     bad_destination_array = []
