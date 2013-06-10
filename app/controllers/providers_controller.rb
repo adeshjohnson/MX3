086abfa04ad054b288e3efdb0e8f731bee7d81dd
aurimass <aurimass@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2013-06-10 10:32:04 +0000

    #7855 - x3 - providers check
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@37778 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -70,6 +70,7 @@ class ProvidersController < ApplicationController
     session[:providers_list_options] = @options
     session[:back] = params
     store_location
+
   end
 
   # in before filter : provider (find_provider)
