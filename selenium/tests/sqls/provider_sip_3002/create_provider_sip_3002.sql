0ff30f3f76bfe3a8cf1bee299c850876712a54d4
ieva <ieva@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2013-06-12 07:02:55 +0000

    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@37825 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- selenium/tests/sqls/provider_sip_3002/create_provider_sip_3002.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `providers` (`id`, `name`, `tech`, `channel`, `login`, `password`, `server_ip`, `port`, `priority`, `quality`, `tariff_id`, `cut_a`, `cut_b`, `add_a`, `add_b`, `device_id`, `ani`, `timeout`, `call_limit`, `interpret_noanswer_as_failed`, `interpret_busy_as_failed`, `register`, `reg_extension`, `terminator_id`, `reg_line`, `hidden`, `use_p_asserted_identity`, `user_id`, `common_use`, `balance`) VALUES
-(3002, 'admin_prov_sip', 'SIP', '', 'admin_prov_sip', 'admin_prov_password', '658.658.658.658', '5060', 1, 1, 1, 0, 0, '', '', 8, 0, 60, 0, 0, 0, 0, '', 0, '', 0, 0, 0, 0, 0.000000000000000);
-
