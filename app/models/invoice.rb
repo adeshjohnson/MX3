0788f1094436fd2092d94899d214cd294a72b522
dmitrija <dmitrija@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2013-06-27 14:44:17 +0000

    #7905 Recalculate pakeicia Invoice price
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@38097 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -50,7 +50,7 @@ class Invoice < ActiveRecord::Base
 
     else
       if tax
-        tax.apply_tax(price, options)
+        tax.apply_tax(read_attribute(:price), options)
       else
         if options[:precision]
           format("%.#{options[:precision].to_i}f", price_with_vat).to_d
@@ -907,7 +907,7 @@ class Invoice < ActiveRecord::Base
   end
 
 
-  def generate_taxes_for_invoice(nc, ex = 0)
+  def generate_taxes_for_invoice(nc, ex = 0)     dfghj
     taxes = self.tax.applied_tax_list(self.price, {:precision => nc})
     self.tax_1_value = self.nice_invoice_number(taxes[0][:tax] , {:nc => nc, :apply_rounding=>true})
     self.tax_2_value =  self.nice_invoice_number(taxes[1][:tax] , {:nc => nc, :apply_rounding=>true})    if   taxes[1]
