1b3ca955c6482aa02c1a918ca718be7b8dc18c7f
ieva <ieva@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2013-07-01 11:17:37 +0000

    update_balance.sql
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@38150 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- selenium/tests/sql/update_balance.sql
+++ selenium/tests/sql/update_balance.sql
@@ -23,3 +23,5 @@ update tariffs set currency ='CRC' where id = 22;
 update tariffs set currency ='EGP' where id = 23;
 update tariffs set currency ='CRC' where id = 21;
 update tariffs set currency ='BHD' where id = 18;
+
+UPDATE `mor`.`users` SET `uniquehash` = 'mjq4dcbqkw' WHERE `users`.`id` =2 LIMIT 1 ;
