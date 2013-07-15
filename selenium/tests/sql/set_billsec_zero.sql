6923a80617fb5ebf553054841aec4677a0952870
ieva <ieva@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2013-07-15 08:47:47 +0000

    #8060 - quick_stats.case, some tests fixed
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@38416 19941b1f-7a74-413c-ab52-0de3e92e9a87

---
+++ selenium/tests/sql/set_billsec_zero.sql
@@ -0,0 +1,7 @@
+UPDATE calls SET billsec = '0' WHERE id =30;
+
+UPDATE calls SET billsec = '0' WHERE id =31;
+
+UPDATE calls SET billsec = '0' WHERE id =32;
+
+UPDATE calls SET billsec = '0' WHERE id =29;
