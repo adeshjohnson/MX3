782d1d0586555c649fa93c7d900de7699b576d76
karolina <karolina@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2013-07-16 14:24:07 +0000

    #8095 EG, KŠ [BUG] Neveikia Minimal Charge for Calls settingas default useryje
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@38482 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1360,10 +1360,15 @@ in before filter : ard (:find_ard)
     params[:user][:cyberplat_active] = params[:cyberplat_active].to_i
     params[:user][:balance] = current_user.convert_curr(params[:user][:balance].to_d)
     params[:user][:recording_hdd_quota] = (params[:user][:recording_hdd_quota].to_d*1048576).to_i
+    
     params[:user][:minimal_charge] = params[:minimal_charge_value].to_i
-    year = params[:minimal_charge_date][:year].to_i
-    month = params[:minimal_charge_date][:month].to_i
+    if params[:minimal_charge_value].to_i != 0 and params[:minimal_charge_date]
+      year = params[:minimal_charge_date][:year].to_i
+      month = params[:minimal_charge_date][:month].to_i
     params[:user][:minimal_charge_start_at] = Time.gm(year, month, 1, 0, 0, 0)
+    elsif params[:minimal_charge_value].to_i == 0
+      params[:user][:minimal_charge_start_at] = nil
+    end
 
     if params[:unlimited].to_i == 1
       params[:user][:credit] = -1
