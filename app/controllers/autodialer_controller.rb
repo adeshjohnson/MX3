f3766277911d8ed9055e24fa457d067ce9a52cbd
robertas <robertas@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2013-07-16 13:31:36 +0000

    #8064 RG AD Export call data to CSV grazina tuscia faila.
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@38475 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- app/controllers/autodialer_controller.rb
+++ app/controllers/autodialer_controller.rb
@@ -504,12 +504,12 @@ class AutodialerController < ApplicationController
       s = []
       
       s << number.number
-      s << number.executed_time.to_s if !number.executed_time.blank?
-      s << number.completed_time.to_s if !number.completed_time.blank?
+      s << nice_date_time(number.executed_time).to_s if !number.executed_time.blank?
+      s << nice_date_time(number.completed_time).to_s if !number.completed_time.blank?
       
       if number.ivr_action_logs and number.ivr_action_logs.size.to_i > 0 
         for action in number.ivr_action_logs
-          s << action.created_at.to_s
+          s << nice_date_time(action.created_at).to_s
           s << action.action_text.to_s.gsub(sep, '')
         end
       end
