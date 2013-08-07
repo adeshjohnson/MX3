5c663229ecc25998e293bef565273e12134a416b
ervinas <ervinas@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2013-08-07 10:57:46 +0000

    #7863 Ervinas (Last calls export failo pavadinime neraso User name)
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@39098 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1002,7 +1002,7 @@ in before filter : user (:find_user_from_id_or_session)
         filename = load_file_through_database(filename) if Confline.get_value("Load_CSV_From_Remote_Mysql").to_i == 1
         if filename
           filename = archive_file_if_size(filename, "csv", Confline.get_value("CSV_File_size").to_d)
-          if (params[:test].to_i == 1) and ((@user != nil) or (Confline.get_value("Show_Usernames_On_Pdf_Csv_Export_Files_In_Last_Calls").to_i != 0))
+          if (params[:test].to_i == 1) and (@user != nil) and (Confline.get_value("Show_Usernames_On_Pdf_Csv_Export_Files_In_Last_Calls").to_i != 0)
             render :text => ("#{nice_user(@user)}_" <<  filename) + test_data.to_s
           elsif params[:test].to_i == 1
             render :text => filename + test_data.to_s  
