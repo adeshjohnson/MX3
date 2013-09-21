d69f0999b1e8e667132991302e8eac6667dfca24
karolina <karolina@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2013-09-21 06:53:31 +0000

    #8424 K [S40] [x3] Subscriptions Start/End date issue
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@40544 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -365,7 +365,7 @@ sql = "SELECT services.name as serv_name , users.first_name, users.last_name, us
     #                                                datetime => 2013-09-01 00:00:00 +1000; datetime.strftime("%Y-%m-%d %H:%M:%S").to_time => 2013-09-01 00:00:00 UTC) 
     # done in ticket #8424
     @user_registered = 0
-    @user_registered = @user.registered_at.try(:at_beginning_of_month).strftime("%Y-%m-%d %H:%M:%S").to_time.to_i if @user.registered_at
+    @user_registered = @user.registered_at.try(:at_beginning_of_month).strftime("%Y-%m-%d %H:%M:%S").to_time.to_i if (@user.registered_at if @user)
     valid_membership_time = @user_registered <= @sub.activation_start.in_time_zone(user_tz).strftime("%Y-%m-%d %H:%M:%S").to_time.to_i
 
     if (((@sub.activation_start < @sub.activation_end) and service.servicetype == "periodic_fee") or service.servicetype == "one_time_fee" or ((service.servicetype == "flat_rate") and (@sub.activation_start < @sub.activation_end))) and valid_membership_time
@@ -461,7 +461,7 @@ sql = "SELECT services.name as serv_name , users.first_name, users.last_name, us
     #                                                datetime => 2013-09-01 00:00:00 +1000; datetime.strftime("%Y-%m-%d %H:%M:%S").to_time => 2013-09-01 00:00:00 UTC) 
     # done in ticket #8424
     @user_registered = 0
-    @user_registered = @user.registered_at.try(:at_beginning_of_month).strftime("%Y-%m-%d %H:%M:%S").to_time.to_i if @user.registered_at
+    @user_registered = @user.registered_at.try(:at_beginning_of_month).strftime("%Y-%m-%d %H:%M:%S").to_time.to_i if (@user.registered_at if @user)
     valid_membership_time = @user_registered <= @sub.activation_start.in_time_zone(user_tz).strftime("%Y-%m-%d %H:%M:%S").to_time.to_i
 
     if (@sub.activation_start <= @sub.activation_end) and valid_membership_time
