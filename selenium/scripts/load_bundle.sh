5f5f7be1d9babaeee30502840023b43111008877
mindaugasm <mindaugasm@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2013-09-23 13:47:07 +0000

    Poka-yoke for bundle loader
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@40590 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- selenium/scripts/load_bundle.sh
+++ selenium/scripts/load_bundle.sh
@@ -4,19 +4,19 @@ FILE=$1
 exec < $FILE
 while read LINE
 do
-    if [ ${LINE:0:1} = '#' ]
-    then
-	# comment
-	echo $LINE
-	echo "<br>"
+    if [ ${LINE:0:1} = '#' ]; then
+		# comment
+		echo $LINE
+		echo "<br>"
     else
-	mysql -u mor -pmor mor < "/home/mor/selenium/sqls/$LINE" 2>&1
-	if [ "$?" != "0" ]; then
-	    echo "<br>Failed to load this SQL file: /home/mor/selenium/sqls/$LINE"
-	    echo "<br>----<br>"
-	    echo `cat /home/mor/selenium/sqls/$LINE`
-	    exit 1;
-	fi 
+   		LINE=`echo $LINE | sed 's/\\r//g'`
+		mysql -u mor -pmor mor < "/home/mor/selenium/sqls/$LINE" 2>&1
+		if [ "$?" != "0" ]; then
+		    echo "<br>Failed to load this SQL file: /home/mor/selenium/sqls/$LINE"
+		    echo "<br>----<br>"
+		    echo `cat /home/mor/selenium/sqls/$LINE`
+		    exit 1;
+		fi 
     fi
 
 done
