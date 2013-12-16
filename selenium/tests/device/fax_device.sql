3a5e6c0bdd8eb1d2b49248f0e9e006f47f61cd7b
ieva <ieva@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2013-12-16 07:28:02 +0000

    tests fixed
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@42933 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- selenium/tests/device/fax_device.sql
+++ selenium/tests/device/fax_device.sql
@@ -1,4 +1,4 @@
 insert into pdffaxes
 (device_id,filename,receive_time,size,uniqueid,fax_sender) VALUES
 (3,'blabla.bla.pdf','2013-08-13 10:00:00',10000,'asfdghrd','Dima'),
-(12,'blabla.bla.pdf','2013-08-13 10:00:00',10000,'asfdghrd','Dima');
+(35,'blabla.bla.pdf','2013-08-13 10:00:00',10000,'asfdghrd','Dima');
