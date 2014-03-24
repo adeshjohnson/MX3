9c2062c9572165d2717c02133b07ee73c6b41799
tomas <tomas@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2014-03-24 09:23:36 +0000

    fax_device.case testas sutvarkytas, ticketui #9338
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@45467 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- selenium/tests/device/fax_device.sql
+++ selenium/tests/device/fax_device.sql
@@ -1,4 +1,6 @@
-insert into pdffaxes
-(device_id,filename,receive_time,size,uniqueid,fax_sender) VALUES
-(3,'blabla.bla.pdf','2013-08-13 10:00:00',10000,'asfdghrd','Dima'),
-(35,'blabla.bla.pdf','2013-08-13 10:00:00',10000,'asfdghrd','Dima');
+insert into pdffaxes (`id`, `device_id`, `filename`, `receive_time`, `size`, `uniqueid`, `fax_sender`, `status`) values
+(304,3,'blabla.bla.pdf','2013-08-13 10:00:00',10000,'asfdghrd','Dima','no_tif'),
+(305,35,'blabla.bla.pdf','2013-08-13 10:00:00',10000,'asfdghrd','Dima','no_tif'),
+(300,3,'pavadinimas','2014-03-18 15:30:45',55,564666465465465446, '37060065432','no_tif'),
+(310,3,'pavadinimas2','2014-03-18 15:35:45',45,5646664365446, '37060065432','no_tif'),
+(315,3,'pavadinimas3','2014-03-18 15:35:45',33,46664365446, '37060065432','no_tif');
