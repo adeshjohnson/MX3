88d2cc6effac0d735036de12bc9b32a952d31235
tomas <tomas@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2014-06-09 09:17:01 +0000

    sqls testam
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@48432 19941b1f-7a74-413c-ab52-0de3e92e9a87

---
+++ selenium/sqls/permissions/reseller/create.sql
@@ -0,0 +1,11 @@
+INSERT INTO `acc_groups` (`id`, `name` ,`group_type`) VALUES ( 12 , 'Reseller_Permissions', 'reseller');
+
+INSERT INTO `acc_group_rights` (`acc_group_id`, `acc_right_id`, `value`) VALUES
+(12, 31, 0),
+(12, 32, 0),
+(12, 33, 0),
+(12, 34, 0),
+(12, 35, 0),
+(12, 37, 0),
+(12, 39, 0),
+(12, 41, 0);
---
+++ selenium/sqls/permissions/reseller/pbx_write.sql
@@ -0,0 +1,2 @@
+delete from acc_group_rights where acc_right_id=41;
+INSERT INTO acc_group_rights (acc_group_id, acc_right_id, value) VALUES (12, 41, 2);
