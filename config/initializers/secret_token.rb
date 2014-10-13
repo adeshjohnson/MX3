c933d4f6e9205ca68458e3ebbc79fec24317f135
ervinas <ervinas@19941b1f-7a74-413c-ab52-0de3e92e9a87>
2014-10-13 12:06:14 +0000

    x3 secret_token
    git-svn-id: http://svn.kolmisoft.com/mor/gui/branches/12@53084 19941b1f-7a74-413c-ab52-0de3e92e9a87

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -8,18 +8,4 @@
 
 require 'securerandom'
 
-def secure_token
-  token_file = Rails.root.join('.secret')
-  if File.exist?(token_file)
-    # Use the existing token.
-    File.read(token_file).chomp
-  else
-    # Generate a new token and store it in token_file.
-    token = SecureRandom.hex(64)
-    File.write(token_file, token)
-    token
-  end
-end
-
-Mor::Application.config.secret_key_base = secure_token
-Mor::Application.config.secret_token = secure_token
+Mor::Application.config.secret_token = SecureRandom.hex(64)
